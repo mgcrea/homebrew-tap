5807ee6ea6cdf0ccb06b02204eba16d517b4d960
Olivier Louvignes <olivier@mg-crea.com>
2026-09-11 20:17:31 +0000

    cupertino 1.21.1

--- Casks/cupertino.rb
+++ Casks/cupertino.rb
@@ -7,8 +7,8 @@
 # The two placeholders below are substituted by that job from the artifact it
 # just built and checksummed.
 cask "cupertino" do
-  version "1.21.0"
-  sha256 "7c62a9b77d9c3235e0e752edf833b4e002ab25ee7757bf4a04aed12fb5b9ba75"
+  version "1.21.1"
+  sha256 "2dfde52d623f38b7a9a23d2e1fb4dfe8dfe88c5594039e164b9ab5076ca05b49"
 
   url "https://github.com/mgcrea/cupertino/releases/download/app-v#{version}/Cupertino.zip",
       verified: "github.com/mgcrea/cupertino/"
